00a3c87a052355d6d9b5c1283f64cb48c56494e6
﻿Chinthaka Rodrigo <chinthaka.ro2003@gmail.com>
2026-03-05 17:09:22 +0530

    Add Vue Router and Pinia to main.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import HomeView from '../views/HomeView.vue'
 import ProductDetailView from '../views/ProductDetailView.vue'
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', component: HomeView },
-        { path: '/product/:id', component: ProductDetailView }
-    ]
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/product/:id', component: ProductDetailView }
+  ]
 })
 
 export default router

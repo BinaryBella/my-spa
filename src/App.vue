66bb3807548cd9fe91e30fbf6b687053469d34df
﻿Chinthaka Rodrigo <chinthaka.ro2003@gmail.com>
2026-03-05 17:41:54 +0530

    Add CartSidebar to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div class="min-h-screen bg-gray-100">
     <NavBar />
     <RouterView />
+    <CartSidebar />
   </div>
 </template>
 
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
+import CartSidebar from './components/CartSidebar.vue'
 </script>
